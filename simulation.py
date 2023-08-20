32605e21519d3aad30a796e1531fdaba5abeecaf
eowsheff <eoweelinck1@sheffield.ac.uk>
2023-08-20 19:12:53 +0200

    WIP - intermediate commit after first results

--- simulation.py
+++ simulation.py
@@ -6,22 +6,22 @@ import numpy as np
 #TODO: fix setting a random seed per simulation
 
 
-def riffle_shuffle_simulation(n_experiments: int, n_cards_in_deck: int, max_n_riffle_shuffle:int, random_seed: int = None) -> list:
+def riffle_shuffle_simulation(n_trials: int, n_cards_in_deck: int, max_n_riffle_shuffle:int, random_seed: int = None) -> list:
     # for testing and reproduction purposes, set the random seed in the scope of this simulation.
     np.random.seed(random_seed)
     
-    # the `results` is a list of lists. For each experiment we run, we append the results of that experiment to the `results` list. 
+    # the `results` is a list of lists. For each trial we run, we append the results of that trial to the `results` list. 
     result: list = []
 
-    # Execute n_experiments
-    for i_experiment in range(n_experiments):
-        # For each experiment create a new deck of cards
+    # Execute n_trials
+    for i_trial in range(n_trials):
+        # For each trial create a new deck of cards
         deck: Deck = Deck().init_new_deck(n_cards_in_deck)
 
-        # The experiment_results holds all the different shuffled Decks of cards. 
-        # The first deck in the experiment_results is the initial deck, in new standard order.
-        experiment_result: list = []
-        experiment_result.append(deck)
+        # The trial_results holds all the different shuffled Decks of cards. 
+        # The first deck in the trial_results is the initial deck, in new standard order.
+        trial_result: list = []
+        trial_result.append(deck)
         
         # Shuffle the deck n number of times, by cutting the deck into two packets and then riffle shuffling the 
         # two packets together, using the gsr.riffle_shuffle function.
@@ -29,93 +29,95 @@ def riffle_shuffle_simulation(n_experiments: int, n_cards_in_deck: int, max_n_ri
             cut_position: int = gsr.get_cut_position(deck)
             left_packet: Deck = deck[:cut_position]
             right_packet: Deck = deck[cut_position:]
+            # l = left_packet.copy()
+            # r = right_packet.copy()
             deck: Deck = gsr.riffle_shuffle(left_packet, right_packet)
-            experiment_result.append(deck)
+            trial_result.append(deck)
             
-        result.append(experiment_result)
+        result.append(trial_result)
     
     return result
 
 
-def a_shuffle_simulation(n_experiments:int, a: int, n_cards_in_deck: int, max_n_shuffle: int, random_seed: int = None) -> list:
+def a_shuffle_simulation(n_trials:int, a: int, n_cards_in_deck: int, max_n_shuffle: int, random_seed: int = None) -> list:
     # for testing and reproduction purposes, set the random seed in the scope of this simulation.
     np.random.seed(random_seed)
     
     result: list = []
     
-    for i_experiment in range(n_experiments):
+    for i_trial in range(n_trials):
         deck: Deck = Deck().init_new_deck(n_cards_in_deck)
         
-        experiment_result: list = []
-        experiment_result.append(deck)
+        trial_result: list = []
+        trial_result.append(deck)
         
         for i_shuffle in range(max_n_shuffle):
             deck: Deck = shuffles.a_shuffle(deck, a)
-            experiment_result.append(deck)
+            trial_result.append(deck)
         
-        result.append(experiment_result)
+        result.append(trial_result)
     
     return result
 
 
-def top_in_at_random_shuffle_simulation(n_experiments: int, n_cards_in_deck: int, random_seed: int = None) -> list:
+def top_in_at_random_shuffle_simulation(n_trials: int, n_cards_in_deck: int, random_seed: int = None) -> list:
     # for testing and reproduction purposes, set the random seed in the scope of this simulation.
     np.random.seed(random_seed)
     
     result: list = []
     
-    for i_experiment in range(n_experiments):
+    for i_trial in range(n_trials):
         deck: Deck = Deck().init_new_deck(n_cards_in_deck)
         bottom_card: int = deck[0]
         
-        experiment_result: list = []
-        experiment_result.append(deck)
+        trial_result: list = []
+        trial_result.append(deck)
         
         while deck[-1] != bottom_card:
             deck = shuffles.top_in_at_random_shuffle(deck)
-            experiment_result.append(deck)
+            trial_result.append(deck)
         
         # perform one more top in at random move for the bottom card 
         deck = shuffles.top_in_at_random_shuffle(deck)
-        experiment_result.append(deck)
+        trial_result.append(deck)
         
-        result.append(experiment_result)
+        result.append(trial_result)
         
     return result
         
 
-def overhand_shuffle_simulation(n_experiments: int, n_cards_in_deck: int, max_n_shuffle: int, p: float = 0.2, random_seed: int = None) -> list:
+def overhand_shuffle_simulation(n_trials: int, n_cards_in_deck: int, max_n_shuffle: int, p: float = 0.2, random_seed: int = None) -> list:
     # for testing and reproduction purposes, set the random seed in the scope of this simulation.
     np.random.seed(random_seed)
         
     result: list = []
     
-    for i_experiment in range(n_experiments):
+    for i_trial in range(n_trials):
         init_deck: Deck = Deck().init_new_deck(n_cards_in_deck)
 
-        experiment_result: list = []
-        experiment_result.append(init_deck)
+        trial_result: list = []
+        trial_result.append(init_deck)
         
         for i_shuffle in range(max_n_shuffle):
             shuffled_deck: Deck = shuffles.overhand_shuffle(init_deck, p=p)
-            experiment_result.append(shuffled_deck)
+            trial_result.append(shuffled_deck)
             new_deck: Deck = Deck()
             new_deck.cards = shuffled_deck.cards
             init_deck = new_deck
         
-        result.append(experiment_result)
+        result.append(trial_result)
 
     return result
         
 
 if __name__ == "__main__":
-    N_EXPERIMENTS = 5
+    N_TRIALS = 5
     N_CARDS = 52
     MAX_RIFFLE_SHUFFLES = 20
     RANDOM_SEED = 2023
-    # When setting the random seeds for a-shuffled and riffle shuffled to the same number, running the same number of experiments should yield the same result
-    r = riffle_shuffle_simulation(n_experiments=N_EXPERIMENTS, n_cards_in_deck=N_CARDS, max_n_riffle_shuffle=MAX_RIFFLE_SHUFFLES, random_seed=RANDOM_SEED)
-    a_r = a_shuffle_simulation(n_experiments=N_EXPERIMENTS, a=2, n_cards_in_deck=N_CARDS, max_n_shuffle=MAX_RIFFLE_SHUFFLES, random_seed=RANDOM_SEED)
+    # When setting the random seeds for a-shuffled and riffle shuffled to the same number, running the same number of trials should yield the same result
+    r = riffle_shuffle_simulation(n_trials=N_TRIALS, n_cards_in_deck=N_CARDS, max_n_riffle_shuffle=MAX_RIFFLE_SHUFFLES, random_seed=RANDOM_SEED)
+    a_r = a_shuffle_simulation(n_trials=N_TRIALS, a=2, n_cards_in_deck=N_CARDS, max_n_shuffle=MAX_RIFFLE_SHUFFLES, random_seed=RANDOM_SEED)
     
     riffle_rising_sequences = {}
     a_rising_sequences = {}
@@ -129,21 +131,21 @@ if __name__ == "__main__":
             riffle_rising_sequences[i].append(riffle_deck.rising_sequences)
             a_rising_sequences[i].append(a_deck.rising_sequences)
     
-    assert len(r) == N_EXPERIMENTS
-    assert len(a_r) == N_EXPERIMENTS
+    assert len(r) == N_TRIALS
+    assert len(a_r) == N_TRIALS
     assert riffle_rising_sequences == a_rising_sequences
     
     
     # number of moves to get the bottom card to the top, should be equal or greater than the number of cards in the deck
-    t_r = top_in_at_random_shuffle_simulation(n_experiments=N_EXPERIMENTS, n_cards_in_deck=N_CARDS, random_seed=RANDOM_SEED)
+    t_r = top_in_at_random_shuffle_simulation(n_trials=N_TRIALS, n_cards_in_deck=N_CARDS, random_seed=RANDOM_SEED)
     number_of_top_in_at_raondom_moves = []
     for exp in t_r:
         number_of_top_in_at_raondom_moves.append(len(exp))
     
-    assert len(r) == N_EXPERIMENTS
+    assert len(r) == N_TRIALS
     assert any([i >= N_CARDS for i in number_of_top_in_at_raondom_moves])
     
     
     MAX_OVERHAND_SHUFFLES = 10000
-    o_r = overhand_shuffle_simulation(n_experiments=N_EXPERIMENTS, n_cards_in_deck=N_CARDS, max_n_shuffle=MAX_OVERHAND_SHUFFLES, p=0.2, random_seed=RANDOM_SEED)
+    o_r = overhand_shuffle_simulation(n_trials=N_TRIALS, n_cards_in_deck=N_CARDS, max_n_shuffle=MAX_OVERHAND_SHUFFLES, p=0.2, random_seed=RANDOM_SEED)
     # write unit tests
--- stats.py
+++ stats.py
@@ -3,53 +3,34 @@ import numpy as np
 from collections import deque
 
 
-def get_random_sequence(first_element: int, last_element: int, random_seed: int = None) -> np.array:
+def eulerian(n: int, r: int) -> int:
     """
-    This function creates a sequence of n elements, without replacement, from `first_element` (included) to `last_element` (included).
-    It samples these elements from a uniform distribution, meaning each element is picked with equal probability.
-
-    E.g.: to create a random sequence of numbers, ranging from 1 (included) to 52 (inclued), this function is called as:
-        get_random_sequence(1, 52)
-        This will produce a list with random numbers such as: [3, 1, 52, .... , 14]
-
-
-    :param  first_element: integer, indicating the first number in the sequence
-            last_element: integer, indicating the last number in the sequence
-            random_seed: integer which sets a random seed to np.random.seed, this allows reproductin the same results when the function is repeated
-
-    :return np.array, containting n elements, samples from a uniform distribution, without replacement
-    """
-    np.random.seed(random_seed)
-
-    n_elem = range(first_element, last_element + 1)
-    n = last_element - (first_element - 1)
-    return np.random.choice(n_elem, n, replace=False)
-
-
-def eulerian(n: int, k: int) -> int:
-    """
-    This function returns the Eulerian number for n, according to the explicit formula here: https://en.wikipedia.org/wiki/Eulerian_number
+    This function returns the Eulerian number for n, according to the explicit formula, see here: https://en.wikipedia.org/wiki/Eulerian_number
 
     :param  n: total number of elements considered (0 to n).
-            k: number of possible permutations with k ascents
+            r: number of possible permutations with r ascents
 
     :return Eulerian number as an integer
     """
-    if n < 0 or k < 0:
+    if n < 0 or r < 0 or r >= n:
         return 0
-    elif n == 0 and k == 0:
+    elif n == 0 and r == 0:
         return 1
-    elif n <= k:
-        return 0
 
     result = 0
-    sign = 1
 
-    for i in range(0, k+1):
-        result += sign * math.comb(n+1, i) * ((k+1-i)**n)
-        sign = sign * (-1)
+    for i in range(0, r+1):
+        result += ((-1)**i) * math.comb(n+1, i) * ((r-i)**n)
 
     return result
+
+
+def uniform(n: int) -> float:
+    return 1.0 / math.factorial(n)
+
+
+def probability_rising_sequence(a, n, k, r):
+    return math.comb(n-r+(a**k), n) / (a**(k*n))
 
 
 def total_variation_distance(P: np.array, Q: np.array) -> float: # update type hinting for parameters to allow collections.deque
@@ -66,9 +47,9 @@ def total_variation_distance(P: np.array, Q: np.array) -> float: # update type h
 
     :return float representing the TVD between the two distributions P and Q
     """
-    if isinstance(P, deque):
+    if isinstance(P, deque) or isinstance(P, list):
         P = np.asarray(P)
-    if isinstance(Q, deque):
+    if isinstance(Q, deque) or isinstance(P, list):
         Q = np.asarray(Q)
 
     return 0.5 * np.sum(np.abs(P - Q))
@@ -88,9 +69,9 @@ def kl_divergence(P: np.array, Q: np.array) -> float: # update type hinting for 
 
     :return float representing the KL divergence between the two distributions P and Q
     """
-    if isinstance(P, deque):
+    if isinstance(P, deque) or isinstance(P, list):
         P = np.asarray(P)
-    if isinstance(Q, deque):
+    if isinstance(Q, deque) or isinstance(P, list):
         Q = np.asarray(Q)
 
-    return sum(P[i] * np.log2(P[i] / Q[i]) for i in range(len(P)))
+    return np.sum(P * np.log2(P / Q))
